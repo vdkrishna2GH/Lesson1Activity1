bd5169de8b6eba4081ec2952eae930257fbf47f2
vdkrishna2GH <vdkrishna2codingal@gmail.com>
2025-08-21 17:01:39 +0530

    Create storingbirthdays.py

---
+++ storingbirthdays.py
@@ -0,0 +1,9 @@
+# Storing birthdays of 5 students
+print("Enter Student names and birthdates:")
+i=0
+while ( i <5):
+  name = input(f"Enter name for student[{i}]:")
+  dob = input(f"Enter dob for student[{i}:]")
+  i+=1
+  print(name)
+  print(dob)
