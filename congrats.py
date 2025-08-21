22a08d165973586a993aa464530e5f6bc2f21414
vdkrishna2GH <vdkrishna2codingal@gmail.com>
2025-08-21 17:14:56 +0530

    Create congrats.py

---
+++ congrats.py
@@ -0,0 +1,5 @@
+# print a congradulations both in uppercase and lowercase 
+print("Enter friend name:")
+name = input()
+print((f"congradulations {name} !").lower())
+print((f"congradulations {name} !").upper())
