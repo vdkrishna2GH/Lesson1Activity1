bf2f3594859d7f3066609016a685357b866f8356
vdkrishna2GH <vdkrishna2codingal@gmail.com>
2025-08-21 16:46:59 +0530

    Create firebrigade.py

---
+++ firebrigade.py
@@ -0,0 +1,12 @@
+#below statement takes input from user
+number=int(input("Enter a number   "))
+
+#below statement is conditional and check for the condition to be true or false
+if number>0:
+  print(number,"is a positive number")
+elif number==0:
+  print(number,"is a neutral number")
+else:
+  print("Negative number")
+a=True
+print(not a)
